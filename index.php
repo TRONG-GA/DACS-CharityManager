4665c47a99aad9ddf8bcfb6e09eba405145c2470
Nguyen Van Trong <23010817@st.phenikaa-uni.edu.vn>
2026-01-28 11:00:15 +0700

    thiết kế layout trang chủ

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'config/db.php';
+$pageTitle = 'Trang chủ - Charity Event';
+include 'includes/header.php';
+include 'includes/navbar.php';
+?>
+
+<!-- Nội dung trang -->
+<h1>Test Phase 2</h1>
+
+<?php include 'includes/footer.php'; ?>
